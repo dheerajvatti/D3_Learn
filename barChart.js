f705405b78456232bd61299feff2b030b2544b7a
Dheeraj <dheerajvatti@gmail.com>
2016-09-16 17:05:00 -0400

    Adding scale and transition

--- barChart.js
+++ barChart.js
@@ -1,77 +1,50 @@
-var w = 300;
-var h = 400;
-var padding = 2;
-var dataset = [5,10,20,30,40,50,40,30,20,10,5];
+	//Width and height
+			var w = 600;
+			var h = 250;
+			var barPadding = 1;
+			
+			var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+							11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+			
+			//Create SVG element
+			var svg = d3.select("body")
+						.append("svg")
+						.attr("width", w)
+						.attr("height", h);
+                        
 
-var svg = d3.select("body").append("svg")
-            .attr("width",w)
-            .attr("height",h);
+			svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length);
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4);
+			   })
+			   .attr("width", w / dataset.length - barPadding)
+			   .attr("height", function(d) {
+			   		return d * 4;
+			   })
+			   .attr("fill", function(d) {
+					return "rgb(0, 0, " + (d * 10) + ")";
+			   });
 
- function colorPicker(v){
-    if(v<=20) { return "#666666";}
-    else if(v>20) {return "#FF0033";}
-}
-
-svg.selectAll("rect")
-   .data(dataset)
-   .enter()
-   .append("rect")
- .attr("x",function(d,i){
-    return i * (w /dataset.length)
-})
-    .attr("y",function(d){
-    return h - (d*4);
-})
-    .attr("width", (w/dataset.length - padding))
-    .attr("height",function(d){
-    return d *4;
-})    
-    .attr("fill",function(d){
-    //return "rgb(0," + (d*10) +",0)"; // using RGB corrdinatesf or synamic coloring
-    return colorPicker(d);
-});
-
-
-//Adding Labels
-svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function (d){ 
-       return d;
-    }) 
-   //calculate the x position by setting it to the left edge of each bar plus half the bar width
-   .attr("x", function(d, i) {
-        return i * (w / dataset.length) + (w / dataset.length - padding) / 2;
-    })
-   .attr("y", function(d) {
-        return h - (d * 4) + 14;  
-    })
-   .attr("font-family", "sans-serif")
-   .attr("font-size", "11px")
-   .attr("fill", "black")
-   .attr("text-anchor", "middle");
-  
- 
-
-//-----------------------------using JSON multi value map for attributes --not working -- check it later
-// .attr({
-//         x:function(d,i) {
-//           return i* (w/ dataset.length);
-//         },
-//         y: function(d){
-//             return h - (d *4);
-//         },
-//         width:w/dataset.length - padding,                
-//         height: function(d){  return d * 4;  },
-//         fill: function(d) {
-//             return red;//"rgb(0," + (d*10) +",0)";
-//         }
-
-// });
-
-
-
-
-
-   
+			svg.selectAll("text")
+			   .data(dataset)
+			   .enter()
+			   .append("text")
+			   .text(function(d) {
+			   		return d;
+			   })
+			   .attr("text-anchor", "middle")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 4) + 14;
+			   })
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "11px")
+			   .attr("fill", "white");
--- scatterPlotWithScale.js
+++ scatterPlotWithScale.js
@@ -1,9 +1,20 @@
-var dataset = [
-                [30, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-              ];
+// var dataset = [
+//                 [30, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+//                 [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+//               ];
 
-var w = 800;
+
+var dataset = [];
+var numDataPoints = 50;
+var xRange = Math.random() * 1000;
+var yRange = Math.random() * 1000;
+for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.round(Math.random() * xRange);
+    var newNumber2 = Math.round(Math.random() * yRange);
+    dataset.push([newNumber1, newNumber2]);
+}
+
+var w = 1000;
 var h = 400;
 var padding = 20;
 
@@ -15,10 +26,18 @@ var svg = d3.select("body")
 
 //scaling
 
+
+// d3.scale.linear() has several other handy methods that deserve a brief mention here:
+
+//------------------
+// nice() — This tells the scale to take whatever input domain that you gave to range() and expand both ends to the nearest round value. From the D3 wiki: “For example, for a domain of [0.20147987687960267, 0.996679553296417], the nice domain is [0.2, 1].” This is useful for normal people, who find it hard to read numbers like 0.20147987687960267.
+// rangeRound() — Use rangeRound() in place of range() and all values output by the scale will be rounded to the nearest whole number. This is useful if you want shapes to have exact pixel values, to avoid the fuzzy edges that may arise with antialiasing.
+// clamp() — By default, a linear scale can return values outside of the specified range. For example, if given a value outside of its expected input domain, a scale will return a number also outside of the output range. Calling .clamp(true) on a scale, however, forces all output values to be within the specified range. Meaning, excessive values will be rounded to the range’s low or high value (whichever is nearest).
+//-------------------
+
 var xScale = d3.scaleLinear()
-                     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-                     //.range([0, w]);
-                     .range([padding, w - padding]);
+								 .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+								 .range([padding, w - padding * 2]);
 
 
 var yScale = d3.scaleLinear()
@@ -28,6 +47,10 @@ var yScale = d3.scaleLinear()
                     // it’s super easy to reverse that, so greater values are higher up, as you would expect. It’s just a matter of changing the output range of yScale
                     //.range([h, 0]);
                     .range([h - padding, padding]);
+
+var rScale = d3.scaleLinear()
+                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                     .range([2, 5]);
 
     
 
@@ -43,27 +66,59 @@ var yScale = d3.scaleLinear()
           return yScale(d[1]);
        })
        .attr("r", function(d) {
-    return Math.sqrt(h - d[1]);
+     return rScale(d[1]);
 });
 
 
 //applying labels
 
-svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function(d) {
-        return d[0] + "," + d[1];
-   })
-   .attr("x", function(d) {
-        return xScale(d[0]);
-   })
-   .attr("y", function(d) {
-         return yScale(d[1]);
-   })
-   .attr("font-family", "sans-serif")
-   .attr("font-size", "11px")
-   .attr("fill", "red");    
+// svg.selectAll("text")
+//    .data(dataset)
+//    .enter()
+//    .append("text")
+//    .text(function(d) {
+//         return d[0] + "," + d[1];
+//    })
+//    .attr("x", function(d) {
+//         return xScale(d[0]);
+//    })
+//    .attr("y", function(d) {
+//          return yScale(d[1]);
+//    })
+//    .attr("font-family", "sans-serif")
+//    .attr("font-size", "11px")
+//    .attr("fill", "red");    
 
 
+
+//==========================================================================
+//Setting up an Axis
+
+//Much like the scale functions, D3’s axes are actually functions whose parameters you define. Unlike scales, when an axis function is called, it doesn’t return a value, 
+//but generates the visual elements of the axis, including lines, labels, and ticks.
+
+//Note that the axis functions are SVG-specific, as they generate SVG elements. Also, axes are intended for use with quantitative scales (as opposed to ordinal ones).
+//===========================================================================
+
+var xAxis = d3.axisBottom()
+                  .scale(xScale)
+                  .ticks(5);
+
+var yAxis = d3.axisLeft()
+                  .scale(yScale)                
+                  .ticks(5);
+                 
+
+
+//Finally, to actually generate the axis and insert all those little lines and labels into our SVG, we must call the xAxis function
+
+svg.append("g")
+    .attr("class", "axis")  //Assign "axis" class
+    .attr("transform", "translate(0," + (h - padding) + ")") //transform the entire axis group, pushing it to the bottom
+    //Note the use of (h - padding), so the group’s top edge is set to h, the height of the entire image, minus the padding value
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
